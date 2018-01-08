ecb410a483b593df9fbec2efea1799667b8ea627
Fernando Henrique Rosa <ferhrosa@gmail.com>
2018-01-08 17:14:47 -0200

    Fixes Firebase dependencies versions
    There was compilation problems with angularfire2 when using firabase 4.8.1, so this changes firebase to 4.8.0 to "fix" the problem.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { environment } from './../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
